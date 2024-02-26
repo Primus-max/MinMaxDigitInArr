3b06f09e2fb91dd468b9a99382951dd1e50afdca
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-26 06:14:53 +0300

    Результат

--- MinMaxDigitInArr.cpp
+++ MinMaxDigitInArr.cpp
@@ -16,13 +16,20 @@ void FillArray(T arr[], int size);
 template <typename T>
 void PrintArray(T arr[], int size);
 
+template<typename T>
+void MinMaxAndIndex(T arr[], int size);
+
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
 	const int sizze = 30;
 	int Array[sizze];
 
 	FillArray(Array, sizze);
 	PrintArray(Array, sizze);
+	MinMaxAndIndex(Array, sizze);
 }
 
 
@@ -33,7 +40,7 @@ void FillArray(T arr[], int size) {
 	for (size_t i = 0; i < size; i++)
 	{
 		arr[i] = Random() % (100 + 50) - 50;
-	}
+	}	
 }
 
 template <typename T>
@@ -42,4 +49,27 @@ void PrintArray(T arr[], int size) {
 	{
 		cout << arr[i] << " ";
 	}
+
+	cout << endl;
+}
+
+template<typename T>
+void MinMaxAndIndex(T arr[], int size) {
+	T min = arr[0], max = arr[0];
+	int indexMax =  0, indexMin = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		if (arr[i] < min){
+			min = arr[i];
+			indexMin = i;
+		}
+			
+		if (arr[i] > max) {
+			max = arr[i];
+			indexMax = i;
+		}
+	}
+	
+	cout << "Минимальный элемент в массиве : " << min << " на позиции : " << indexMin << endl;
+	cout << "Максимальный элемент в массиве : " << max << " на позиции : " << indexMax << endl;
 }
